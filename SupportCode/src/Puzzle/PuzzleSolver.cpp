6887cba5f62afdcdd5882237d7e25c3a90fdbb36
DianeRay <ruikang.dai@gmail.com>
2016-02-05 17:04:55 -0500

    Searching algorithms of N-puzzle
    This code is possible to run on Mac/Linux/Windows

---
+++ SupportCode/src/Puzzle/PuzzleSolver.cpp
@@ -0,0 +1,13 @@
+#include "Puzzle/PuzzleSolver.hpp"
+
+bool PuzzleSolver::Solve(Puzzle *puzzle, 
+			 PuzzleHeuristic *h, 
+			 Puzzle::Board *board, 
+			 const double tmax, 
+			 std::vector<Puzzle::Move> *moves)
+{
+    //add your implementation
+    return false;
+    
+}
+
